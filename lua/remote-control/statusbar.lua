997320fb53437546ce223ef32e013af766cc7881
Thomas Grund <info@mrreason.org>
2025-10-02 14:59:28 +0200

    Add segment loop

--- lua/remote-control/statusbar.lua
+++ lua/remote-control/statusbar.lua
@@ -6,10 +6,19 @@ local pluginLibUv = require("losc.src.losc.plugins.udp-libuv")
 
 local config = require("remote-control.config")
 
+local ns = vim.api.nvim_create_namespace("statusbar")
+
 local status_buf
 local status_win_id
+local bars_number_ext_mark_id
+local total_number_of_bars
+local json_file_name
 
-local function updateStatusBar(n)
+M.activePos = nil
+M.lastGlobalRotlPos = nil
+M.resetGlobalRotl = function() end
+
+local function updateSegments(n)
 	local cur_win = vim.api.nvim_get_current_win()
 
 	-- Switch to status window
@@ -36,15 +45,35 @@ local function updateStatusBar(n)
 
 	-- replace character with ";"
 	vim.api.nvim_buf_set_text(status_buf, row, col - 1, row, col + 2, { "󰄮" })
+	M.activePos = n
+
+	M.resetGlobalRotl()
 
 	-- leave cursor at that position
 	vim.api.nvim_win_set_cursor(0, { 1, 0 })
 	vim.api.nvim_set_current_win(cur_win)
 end
 
-local function setStatus(left, middle, total_count, hl_song_name, hl_checkbox)
+local function updateCurrentBar(relativePos, startBar, endBar)
+	local mark = vim.api.nvim_buf_get_extmark_by_id(status_buf, ns, bars_number_ext_mark_id, { details = true })
+	local row, col, details = mark[1], mark[2], mark[3]
+	-- local barContent = " " .. startBar .. "/" .. relativePos .. "/" .. endBar .. "/" .. totalNumberOfBars .. " "
+	local barContent = " " .. relativePos .. "/" .. startBar .. "-" .. endBar .. " | " .. total_number_of_bars .. " "
+
+	if details and details.end_col then
+		vim.api.nvim_buf_set_text(status_buf, row, col, row, details.end_col, { barContent })
+
+		vim.api.nvim_buf_set_extmark(status_buf, ns, row, col, {
+			id = bars_number_ext_mark_id,
+			end_col = col + #barContent,
+			hl_group = "TLBarsNumber",
+		})
+	end
+end
+
+local function setStatus(artist, songName, barsNumber, total_count, hl_song_name, hl_bars_number, hl_checkbox)
 	-- build full line text
-	local line = " " .. left .. "  " .. middle .. "  "
+	local line = " " .. artist .. "  " .. songName .. "  " .. barsNumber .. "  "
 
 	-- add empty/filled markers
 	for i = 1, total_count do
@@ -58,21 +87,25 @@ local function setStatus(left, middle, total_count, hl_song_name, hl_checkbox)
 	vim.api.nvim_buf_clear_namespace(status_buf, -1, 0, -1)
 
 	-- highlight sections
-	local ns = vim.api.nvim_create_namespace("statusbar")
 
 	vim.api.nvim_set_hl(0, "TLArtistName", { bg = "#346beb", foreground = "#000000" })
-	vim.api.nvim_set_hl(0, "TLSongName", hl_song_name)
+	vim.api.nvim_set_hl(0, "TLSongName", { bg = "#987654", foreground = "#000000" })
+	vim.api.nvim_set_hl(0, "TLBarsNumber", { bg = "#333333" })
 	vim.api.nvim_set_hl(0, "TLCheckBox", hl_checkbox)
 
 	vim.api.nvim_buf_set_extmark(status_buf, ns, 0, 0, {
-		end_col = #left + 2,
+		end_col = #artist + 2,
 		hl_group = "TLArtistName",
 	})
-	vim.api.nvim_buf_set_extmark(status_buf, ns, 0, #left + 2, {
-		end_col = #left + 4 + #middle,
+	vim.api.nvim_buf_set_extmark(status_buf, ns, 0, #artist + 2, {
+		end_col = #artist + 4 + #songName,
 		hl_group = "TLSongName",
 	})
-	vim.api.nvim_buf_set_extmark(status_buf, ns, 0, #left + 1 + #middle + 1 + 2, {
+	bars_number_ext_mark_id = vim.api.nvim_buf_set_extmark(status_buf, ns, 0, #artist + 1 + #songName + 3, {
+		end_col = #artist + 6 + #songName + #barsNumber,
+		hl_group = "TLBarsNumber",
+	})
+	vim.api.nvim_buf_set_extmark(status_buf, ns, 0, #artist + 1 + #songName + 1 + #barsNumber + 2, {
 		end_col = #line, -- until EOL
 		hl_group = "TLCheckBox",
 	})
@@ -88,12 +121,14 @@ local function startRemoteControlServer(host, port)
 			local title = msg[2]
 			local size = msg[4]
 			local color = msg[6]
+			total_number_of_bars = msg[8]
+			json_file_name = msg[10]
 
 			if title and size and color then
 				-- M.set_status("MrReason", title, size, { bg = color, foreground = "#000000" }, {
 				-- 	fg = color,
 				-- })
-				setStatus("MrReason", title, size, {}, {})
+				setStatus("MrReason", title, "N/A", size, {}, {}, {})
 			end
 		end)
 	end)
@@ -104,7 +139,20 @@ local function startRemoteControlServer(host, port)
 			local buttonIndex = msg[2]
 
 			if buttonIndex then
-				updateStatusBar(buttonIndex + 1)
+				updateSegments(buttonIndex + 1)
+			end
+		end)
+	end)
+
+	osc:add_handler("/pulsar/currentBar", function(data)
+		vim.schedule(function()
+			local msg = data.message
+			local relativePos = msg[2]
+			local startBar = msg[4]
+			local endBar = msg[6]
+
+			if startBar and endBar then
+				updateCurrentBar(relativePos, startBar, endBar)
 			end
 		end)
 	end)
@@ -116,7 +164,37 @@ local function startClient()
 	local udp = pluginSocket.new({ sendAddr = "localhost", sendPort = 57120 })
 	local osc = losc.new({ plugin = udp })
 
-	vim.keymap.set("n", "<leader>ts", function()
+	vim.keymap.set("n", "<leader>tl", function()
+		vim.ui.input({ prompt = "Enter loop range: " }, function(input)
+			local loopStart
+			local loopEnd
+
+			if input ~= nil then
+				local parts = {}
+				for part in string.gmatch(input, "([^%-]+)") do
+					table.insert(parts, part)
+				end
+
+				if parts[2] == nil then
+					loopStart = parts[1] - 1
+					loopEnd = parts[1]
+				else
+					loopStart = parts[1] - 1
+					loopEnd = parts[2]
+				end
+
+				osc:send(losc.new_message({
+					address = "/pulsar/loop-segments",
+					types = "sii",
+					json_file_name,
+					loopStart,
+					loopEnd,
+				}))
+			end
+		end)
+	end, { desc = "Loop song segments" })
+
+	vim.keymap.set("n", "<leader>tp", function()
 		local count = vim.v.count1 - 1 -- if no number given, defaults to 1
 
 		osc:send(losc.new_message({
@@ -130,7 +208,34 @@ local function startClient()
 			types = "i",
 			count,
 		}))
+
+		vim.fn["repeat#set"](vim.api.nvim_replace_termcodes((count + 1) .. "<leader>tp", true, true, true), -1)
 	end, { desc = "Send remote control index" })
+
+	vim.keymap.set("n", "<leader>tR", function()
+		local count = vim.v.count -- if no number given, defaults to 1
+
+		M.lastGlobalRotlPos = M.activePos
+
+		osc:send(losc.new_message({
+			address = "/pulsar/global-rotl",
+			types = "i",
+			count,
+		}))
+
+		vim.fn["repeat#set"](vim.api.nvim_replace_termcodes(count .. "<leader>tR", true, true, true), -1)
+	end, { desc = "Set global rotL value" })
+
+	M.resetGlobalRotl = function()
+		if M.lastGlobalRotlPos ~= nil and M.lastGlobalRotlPos ~= M.activePos then
+			M.lastGlobalRotlPos = nil
+			osc:send(losc.new_message({
+				address = "/pulsar/global-rotl",
+				types = "i",
+				0,
+			}))
+		end
+	end
 end
 
 local function createStatusBar()
@@ -190,7 +295,7 @@ function M.create()
 	-- 		fg = "#9842f5",
 	-- 	})
 	--
-	setStatus("MrReason", "N/A", 1, {}, {})
+	setStatus("MrReason", "N/A", "N/A", 1, {}, {}, {})
 	startClient()
 end
 
